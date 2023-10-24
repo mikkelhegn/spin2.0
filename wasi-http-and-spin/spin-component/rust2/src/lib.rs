6a099dac6198d173906f3cbf924fe0fda8e929c7
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2023-10-24 11:28:40 +0200

    wasi and Spin components
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

---
+++ wasi-http-and-spin/spin-component/rust2/src/lib.rs
@@ -0,0 +1,12 @@
+use spin_sdk::http::{IntoResponse, Request};
+use spin_sdk::http_component;
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn handle_rust2(req: Request) -> anyhow::Result<impl IntoResponse> {
+    println!("{:?}", req.headers);
+    Ok(http::Response::builder()
+        .status(200)
+        .header("content-type", "text/plain")
+        .body("Hello, Fermyon")?)
+}
